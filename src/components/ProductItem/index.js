45fdfed3dfb2d835a6ea67341873ce6b0c901ce8
LAPTOP-TV0RNLQ2\INFOFLOW17 <ipan@infoflow.co.id>
2019-07-29 22:16:46 +0700

    add state to product list use in product item

--- src/components/ProductItem/index.js
+++ src/components/ProductItem/index.js
@@ -2,33 +2,50 @@ import React from 'react'
 
 class ProductItem extends React.Component {
   constructor(props) {
-    super(props)
-    this.itemClicked = this.itemClicked.bind(this)
-    this.itemReset = this.itemReset.bind(this)
-    this.state = { isClicked: false }
+    super(props);
+    this.itemClicked = this.itemClicked.bind(this);
+    this.state = { width: 0, height: 0 };
+    this.frontRef = React.createRef();
+    this.backRef = React.createRef();
   }
-  itemClicked(e) {
-    this.setState({ isClicked: !this.state.isClicked })
+  itemClicked(i) {
+    this.resizeBack()
+    console.log(i)
+    this.props.onClick(i)
   }
-  itemReset(e) {
-    this.setState({ isClicked: false })
+  resizeBack() {
+    // const { width, height } = this.state
+    this.backRef.current.style.height = this.frontRef.current.offsetHeight + "px";
+  }
+  componentDidMount() {
+    window.addEventListener("resize", this.updateWindowDimension())
+  }
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.updateWindowDimension())
+  }
+  updateWindowDimension() {
+    this.setState({ width: window.outerWidth, height: window.outerHeight })
   }
   render() {
-    const { name, image } = this.props;
-
+    const { name, image, id } = this.props;
     return (
       <div className="column is-3">
-        <div className={this.state.isClicked ? 'flip-container flip' : 'flip-container'} onClick={this.itemClicked} onMouseLeave={this.itemReset}>
+        <div className={this.props.active ? "flip-container flip" : "flip-container"} onClick={() => this.itemClicked(id)}>
           <div className="flipper">
-            <div className="box front">
-              {name}
-              <figure class="image">
+            <div className="front" ref={this.frontRef}>
+              <figure className="image">
                 <img src={image} alt="" />
               </figure>
             </div>
-            <div className="box back">
-              back
-              </div>
+
+            <div className="back" ref={this.backRef}>
+              {name}
+            </div>
+            <div className="inner_box ghost">
+              <figure className="image">
+                <img src={image} alt="" />
+              </figure>
+            </div>
           </div>
         </div>
       </div>
--- src/components/ProductList/index.js
+++ src/components/ProductList/index.js
@@ -2,17 +2,55 @@ import React from 'react'
 import ProductItem from '../ProductItem'
 
 class ProductList extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      active: []
+    }
+    this.onItemClick = this.onItemClick.bind(this)
+  }
+  onItemClick(i) {
+    this.reset(i)
+  }
+  componentDidMount() {
+    this.reset()
+  }
+  reset(i = undefined) {
+    let active = []
+    this.props.products.forEach(product => {
+      active.push(false)
+    })
+    if (i !== undefined) {
+      active[i] = true
+    }
+    this.setState({
+      active: active
+    })
+  }
   render() {
     const rows = [];
-    this.props.products.forEach(product => {
+    this.props.products.forEach((product, i) => {
       if (
         product.name.toLowerCase().includes(this.props.filterText.toLowerCase())
       ) {
-        rows.push(<ProductItem {...product} key={product.name} />);
+        rows.push(< ProductItem {
+          ...product
+        }
+          active={
+            this.state.active[i]
+          }
+          onClick={
+            () => this.onItemClick(i)
+          }
+          key={i}
+          id={i}
+        />);
       }
     });
-    return <div className="columns is-multiline is-mobile">{rows}</div>;
-  }
-}
-
-export default ProductList
+    return <div className="columns is-multiline is-mobile" > {
+      rows
+    } < /div>;
+                                        }
+                                      }
+                                    
+    export default ProductList
